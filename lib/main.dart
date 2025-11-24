51cd6b5e68644c5b3825d120702ad885e45a289f
CheremosCJ <cheremos.us@gmail.com>
2025-11-24 09:10:05 -0600

    new files added 24 11 25

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ void main() {
 }
 
 class HabitTrackerApp extends StatelessWidget {
+  const HabitTrackerApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
